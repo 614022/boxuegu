c443fdd1b3f0206ff7816ea7f7d63586bf2a8838
wenge <337356772@qq.com>
2017-04-11 10:35:40 +0800

    qw

--- js/course/course_add_step1.js
+++ js/course/course_add_step1.js
@@ -1,3 +1,21 @@
-define([], function() {
+define(['jquery', 'common', 'header', 'aside', 'nprogress', 'loading', 'template', 'jqueryForm'], 
+	function($, common, undefined, undefined, nprogress, loading, template, undefined) {
 	
+	/**
+	 * 1、获取url查询字符串中的cs_id
+	 * 2、利用这个cs_id请求接口获取当前课程的基本信息，渲染页面进行数据回显
+	 * 3、初始化表单提交插件
+	 * */
+	
+	// 1、获取url查询字符串中的cs_id
+	var csId = common.parseSearch('cs_id');
+	
+	// 2、利用这个cs_id请求接口获取当前课程的基本信息，渲染页面进行数据回显
+	$.get('/v6/course/basic', { cs_id: csId }, function(data) {
+		if(data.code == 200) {
+			$('.steps').html(template('add-step1-tpl', data.result));
+		}
+	});
+	
+	nprogress.done();
 });
